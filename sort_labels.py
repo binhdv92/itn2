813e2f38306b270d035f3af47abf146bbb692f96
Binh Duong Van <binhdv92@gmail.com>
2019-12-10 11:05:57 +0700

    modify done

--- sort_labels.py
+++ sort_labels.py
@@ -6,16 +6,13 @@ import numpy as np
 
 #with open("C:\\itn2\images2\\500237051_(1).txt",mode="r") as f:
 #    data = f.readcsv
-#
-nametable=["images2/500237051_(1).txt",
-           "images2/500237051_(2).txt"
-           "images2/500237051_(3).txt"
-           "images2/500237051_(4).txt"
-           "images2/500237051_(5).txt"
-           "images2/500237051_(6).txt"]
            
-tempname="images2/500237051_(1).txt"
-
+nametable=["images2/500390261_(1).txt",
+           "images2/500390261_(2).txt",
+           "images2/500390261_(3).txt",
+           "images2/500390261_(4).txt",
+           "images2/500390261_(5).txt"]
+           
 for tempname in nametable:
     print(tempname)
     data = pd.read_csv(tempname,delimiter=' ',encoding='utf-8',header=None,prefix='var')
